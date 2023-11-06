98d120d04e540d8b1b35b9e52c0e3b7b920821dc
Mike <mike.kangaroo.world@gmail.com>
2023-11-06 09:43:54 +0900

    [feat] v-model (#2)
    Co-authored-by: OKESTRO <mike@OKESTROui-MacBookPro-2.local>

---
+++ src/components/MyChildren.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>children input : <AInput :value="computedValue" /></div>
+</template>
+
+<script setup lang="ts">
+import { Input as AInput } from "ant-design-vue";
+import { ChangeEventHandler } from "ant-design-vue/es/_util/EventInterface";
+import { computed } from "vue";
+
+interface Props {
+  inputValue: string;
+}
+const props = defineProps<Props>();
+
+interface Emits {
+  (e: "updateInput", value: string): void;
+}
+const emit = defineEmits<Emits>();
+
+const computedValue = computed({
+  get: () => props.inputValue,
+  set: (value) => emit("updateInput", value),
+});
+</script>
---
+++ src/components/MyChildrenVM.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    children vm input :
+    <AInput :value="computedValue" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { Input as AInput } from "ant-design-vue";
+import { computed } from "vue";
+
+interface Props {
+  modelValue: string;
+  // inputState: string;
+}
+const props = defineProps<Props>();
+
+interface Emits {
+  (e: "update:modeValue", value: string): void;
+  // (e: "update:inputState", value: string): void;
+}
+const emit = defineEmits<Emits>();
+const computedValue = computed({
+  get: () => props.modelValue,
+  set: (value) => emit("update:modeValue", value),
+});
+</script>
