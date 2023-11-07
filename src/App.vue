f7695ea9011c54ea9e202bf5ce42fd7bc8ce1c67
OKESTRO <mike@OKESTROui-MacBookPro-2.local>
2023-11-07 11:30:29 +0900

    [feat] create logic for avoid duplicate request

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,54 @@
 <template>
-  <!-- 여기가 부모 -->
   <div class="app-container">
-    <!-- 자식 -->
-    <MyChildren :input-value="inputValue" @update-input="updateInputValue" />
-    <!-- V-MODEL -->
-    <MyChildrenVM v-model="vmInputValue" />
-    <MyChildrenVM :modelValue="vmInputValue" @update:modelValue="" />
-    <MyChildrenVM :input-State="vmInputValue" @update:input-State="" />
+    <div class="button-continer">
+      <!-- <Button ref="buttonElement" :loading="isLoading" @click="fetchProducts">
+        Get Products
+      </Button> -->
+      <Button @click="fetchProducts"> query once Products </Button>
+    </div>
+    <div class="product-list">{{ result }}</div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { Button } from "ant-design-vue";
+import { getProdcuts } from "./api/getProducts";
 import { ref } from "vue";
-import MyChildren from "./components/MyChildren.vue";
-import MyChildrenVM from "./components/MyChildrenVM.vue";
 
-const inputValue = ref("");
-const updateInputValue = (value: string) => {
-  console.log("parent updateinputvalue", value);
+// const isLoading = ref(false);
+const result = ref<[{ name: string }]>();
+const fetchProducts = () => {
+  // isLoading.value = true;
+  console.log("click!");
+  getProdcuts().then((data) => {
+    result.value = data;
+    console.log("res", result.value);
+  });
+  // .finally(() => (isLoading.value = false));
 };
-
-const vmInputValue = ref("");
 </script>
 
 <style scoped>
 .app-container {
   width: 80%;
   padding: 40px;
+
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
 }
 
-.data-container {
-  padding: 40px;
+.product-list {
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+}
+
+.submit-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  gap: 8px;
 }
 </style>
